3414358bda0f4ef900b115373898b339cabf2966
Sebastian Błażejewski <74263390+smootny@users.noreply.github.com>
2024-03-24 20:47:29 +0100

    added photo compare function and upload button

--- flex-flow-freeze/src/app/image-comparison/image-comparison.component.ts
+++ flex-flow-freeze/src/app/image-comparison/image-comparison.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-image-comparison',
   templateUrl: './image-comparison.component.html',
   styleUrls: ['./image-comparison.component.css']
 })
-export class ImageComparisonComponent {
+export class ImageComparisonComponent implements OnInit {
+
+  @ViewChild('slider') slider!: ElementRef;
+  @ViewChild('image') image!: ElementRef;
+  @ViewChild('dragLine') dragLine!: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+  }
+
+  // This function will be triggered when the input range value changes
+  adjustImage(event: any): void {
+    const sliderVal = event.target.value;
+    this.renderer.setStyle(this.dragLine.nativeElement, 'left', `${sliderVal}%`);
+    this.renderer.setStyle(this.image.nativeElement, 'width', `${sliderVal}%`);
+  }
 
 }
